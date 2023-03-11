a394adfbb34527148dd50fef5fc1084a2abb3ed6
Deepanjan Nag <subscriptions@deepanjannag.com>
2023-03-10 21:37:58 -0500

    Some changes

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -12,11 +12,11 @@ namespace Entities
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
-            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
-        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+        //    modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        //}
 
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
